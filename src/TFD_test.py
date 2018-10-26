11b9c4f0c8a90b84a350c5e1630a71bc133d3b8f
RMeli <rocco.meli@chem.ox.ac.uk>
2018-10-26 16:24:20 +0100

    build lattice graph

--- src/TFD_test.py
+++ src/TFD_test.py
@@ -18,20 +18,41 @@ def build_path_graph(n):
 
     return G
 
-def test_build_path_graph():
+def test_build_path_graph(n = 10):
 
     with pytest.raises(ValueError):
         G = build_path_graph(1)
 
-    nodes = 10
+    G = build_path_graph(n)
 
-    G = build_path_graph(nodes)
+    assert G.number_of_nodes() == n
+    assert G.number_of_edges() == n - 1
 
-    assert G.number_of_nodes() == nodes
-    assert G.number_of_edges() == nodes - 1
+def build_lattice_graph(n):
+    """
+    Build lattice graph with n*n nodes
+    """
 
-def lattice_graph(n):
-    pass
+    if n < 2:
+        raise ValueError
+
+    G = nx.Graph()
+
+    G.add_nodes_from([i for i in range(n * n)])
+
+    for i in range(n):
+        for j in range(n - 1):
+            idx = i * n + j
+
+            G.add_edge(idx, idx + 1)
+
+    for i in range(n - 1):
+        for j in range(n):
+            idx = i * n + j
+
+            G.add_edge(idx, idx + n)
+
+    return G
 
 def test_topological_fractal_dimension():
     tfd = lambda G: TFD.topological_fractal_dimension(G)
@@ -50,7 +71,12 @@ def test_topological_fractal_dimension():
     # assert tdf(ErdosRenyiGraph) ==
 
 if __name__ == "__main__":
-        G = build_path_graph(10)
+        G = build_path_graph(5)
+        plt.figure()
+        nx.draw(G, with_labels=True)
+        plt.show()
+
+        G = build_lattice_graph(3)
         plt.figure()
         nx.draw(G, with_labels=True)
         plt.show()
