f9eabd118dd9719ea4c272982f53f11fea089132
RMeli <rocco.meli@chem.ox.ac.uk>
2018-10-26 13:48:39 +0100

    build path graph

--- src/TFD_test.py
+++ src/TFD_test.py
@@ -1,14 +1,35 @@
 import TFD
 
+import matplotlib.pyplot as plt
 import networkx as nx
 import pytest
+
+def build_path_graph(n):
+    """
+    Build path graph of length n.
+    """
+
+    if n < 2:
+        raise ValueError
+
+    G = nx.Graph()
+
+    G.add_nodes_from([i for i in range(n)])
+
+    for i in range(n-1):
+        G.add_edge(i, i+1)
+
+    return G
+
+def lattice_graph(n):
+    pass
 
 def test_topological_fractal_dimension():
     tfd = lambda G: TFD.topological_fractal_dimension(G)
 
-    LinearGraph = None
+    PathGraph = build_path_graph(10)
 
-    #assert tfd(LinearGraph) == pytest.approx(1)
+    #assert tfd(PathGraph) == pytest.approx(1)
 
     LatticeGraph = None
 
@@ -18,3 +39,9 @@ def test_topological_fractal_dimension():
     ErdosRenyiGraph = nx.gnm_random_graph(nodes, edges)
 
     # assert tdf(ErdosRenyiGraph) ==
+
+if __name__ == "__main__":
+        G = build_path_graph(10)
+        plt.figure()
+        nx.draw(G, with_labels=True, font_weight='bold')
+        plt.show()
