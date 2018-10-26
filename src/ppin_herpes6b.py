473eb7e477473e51737201b8b801e82b733f0ece
Florian <florian.stoertz@gmail.com>
2018-10-26 23:29:52 +0100

    edited graph labelling

--- src/ppin_herpes6b.py
+++ src/ppin_herpes6b.py
@@ -3,13 +3,25 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # import BioGrid file
-df_herpes = pd.read_csv("../biograd-organism/BIOGRID-ORGANISM-Human_Herpesvirus_6B-3.5.165.tab2.txt", sep='\t',header=(0))
-col1_name = 'BioGRID ID Interactor A'
-col2_name = 'BioGRID ID Interactor B'
+# BIOGRID-ORGANISM-Human_Herpesvirus_8-3.5.165.tab2.txt
+# BIOGRID-ORGANISM-Human_Herpesvirus_6B-3.5.165.tab2.txt
+df_herpes   = pd.read_csv("../biograd-organism/BIOGRID-ORGANISM-Human_Herpesvirus_6B-3.5.165.tab2.txt", sep='\t', header=(0))
+colA_name   = 'BioGRID ID Interactor A'
+colB_name   = 'BioGRID ID Interactor B'
+colOffA_name = 'Official Symbol Interactor A'
+colOffB_name = 'Official Symbol Interactor B'
 
-# TODO save edgelist file, save official symbols file
+# TODO: save edgelist file, save official symbols file
+
 
 # draw graph
-graph = nx.from_pandas_edgelist(df_herpes[[col1_name,col2_name]], col1_name, col2_name)
+interactorA     = [val for sublist in df_herpes[[colA_name]].values for val in sublist] # flatten the list like this
+officialSymbolA = [val for sublist in df_herpes[[colOffA_name]].values for val in sublist]
+interactorB     = [val for sublist in df_herpes[[colB_name]].values for val in sublist]
+officialSymbolB = [val for sublist in df_herpes[[colOffB_name]].values for val in sublist]
+
+plt.figure(figsize=(10,8))
+graph = nx.from_pandas_edgelist(df_herpes[[colA_name,colB_name]], colA_name, colB_name) # need to give a directionality here - just ignore
+graph = nx.relabel_nodes(graph, dict(zip(interactorA+interactorB, officialSymbolA+officialSymbolB)))
 nx.draw_networkx(graph)
 plt.show()
