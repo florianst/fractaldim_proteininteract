162e993c468e43420d6b584414867e884dfc1380
RMeli <rocco.meli@chem.ox.ac.uk>
2018-10-29 16:41:21 +0000

    more tests

--- src/graphs_test.py
+++ src/graphs_test.py
@@ -7,17 +7,20 @@ def test_build_path_graph(n = 10):
     with pytest.raises(ValueError):
         G = graphs.build_path_graph(1)
 
-    G = graphs.build_path_graph(n)
+    for i in range(2, n):
+        G = graphs.build_path_graph(i)
 
-    assert G.number_of_nodes() == n
-    assert G.number_of_edges() == n - 1
+        assert G.number_of_nodes() == i
+        assert G.number_of_edges() == i - 1
+
 
 def test_build_lattice_graph(n = 5):
 
     with pytest.raises(ValueError):
         G = graphs.build_lattice_graph(1)
 
-    G = graphs.build_lattice_graph(n)
+    for i in range(2, n):
+        G = graphs.build_lattice_graph(i)
 
-    assert G.number_of_nodes() == n * n
-    assert G.number_of_edges() == 2 * n**2 - 2 * n
+        assert G.number_of_nodes() == i * i
+        assert G.number_of_edges() == 2 * i**2 - 2 * i
